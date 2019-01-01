82a99b481e0793c8a171ab027c04ec246c601173
Adolfo Builes <builes.adolfo@gmail.com>
2019-01-01 14:25:50 -0500

    Add vcr test with ynab.

--- test/ynabit/sources/sources_test.exs
+++ test/ynabit/sources/sources_test.exs
@@ -1,28 +1,35 @@
 defmodule Ynabit.SourcesTest do
   use Ynabit.DataCase
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   alias Ynabit.Sources
+  alias Ynabit.Sources.Notification
+
+  @email %{
+    "bcc" => %{
+      "email" => "someuser@ynabit.com",
+      "full" => "someuser@ynabit.com",
+      "host" => "ynabit.com",
+      "name" => nil,
+      "token" => "someuser"
+    },
+    "text" =>
+      "--\Tom Doe\n\n\n---------- Forwarded message ---------\nFrom: <BANCO_DAVIVIENDA@davivienda.com>\nDate: Tue, Nov 27, 2018 at 7:33 PM\nSubject: DAVIVIENDA\nTo: <tomdoe@gmail.com>\n\n\nApreciado(a) Tom Doe:\n\nLe  informamos que se ha registrado el siguiente movimiento de su Tarjeta\nCrédito terminada en ****2020:\n\nFecha: 2018/11/28\nHora: 5:32:56\nValor Transacción: 7,409\nClase de Movimiento: Compra\nRespuesta: Aprobado(a)\nLugar de Transacción: UBER   *TRIP-WL2SO\n\nBANCO DAVIVIENDA\nAVISO LEGAL : Este mensaje es confidencial, puede contener\ninformación privilegiada y no puede ser usado ni divulgado por\npersonas distintas de su destinatario. Si obtiene esta transmisión\npor error, por favor destruya su contenido y avise a su remitente.\nesta prohibida su retención, grabación, utilización, aprovechamiento\no divulgación con cualquier propósito. Este mensaje ha sido sometido\na programas antivirus. No obstante, el BANCO DAVIVIENDA S.A. y sus\nFILIALES   no\nasumen ninguna responsabilidad por eventuales daños generados por\nel recibo y el uso de este material, siendo responsabilidad del destinatario\nverificar con sus propios medios la existencia de virus u otros\ndefectos. El presente correo electrónico solo refleja la opinión de\nsu Remitente y no representa necesariamente la opinión oficial del\nBANCO DAVIVIENDA S.A. y sus FILIALES  o de sus Directivos\n"
+  }
+  @valid_attrs %{payload: %{}, processed: true, raw: @email}
+  @update_attrs %{payload: %{}, processed: false, raw: @email}
+  @invalid_attrs %{payload: nil, processed: nil, raw: nil}
+
+  def notification_fixture(attrs \\ %{}) do
+    {:ok, notification} =
+      attrs
+      |> Enum.into(@valid_attrs)
+      |> Sources.create_notification()
+
+    notification
+  end
 
   describe "notifications" do
-    alias Ynabit.Sources.Notification
-
-    @email %{
-      "text" =>
-        "--\Tom Doe\n\n\n---------- Forwarded message ---------\nFrom: <BANCO_DAVIVIENDA@davivienda.com>\nDate: Tue, Nov 27, 2018 at 7:33 PM\nSubject: DAVIVIENDA\nTo: <tomdoe@gmail.com>\n\n\nApreciado(a) Tom Doe:\n\nLe  informamos que se ha registrado el siguiente movimiento de su Tarjeta\nCrédito terminada en ****2020:\n\nFecha: 2018/11/28\nHora: 5:32:56\nValor Transacción: 7,409\nClase de Movimiento: Compra\nRespuesta: Aprobado(a)\nLugar de Transacción: UBER   *TRIP-WL2SO\n\nBANCO DAVIVIENDA\nAVISO LEGAL : Este mensaje es confidencial, puede contener\ninformación privilegiada y no puede ser usado ni divulgado por\npersonas distintas de su destinatario. Si obtiene esta transmisión\npor error, por favor destruya su contenido y avise a su remitente.\nesta prohibida su retención, grabación, utilización, aprovechamiento\no divulgación con cualquier propósito. Este mensaje ha sido sometido\na programas antivirus. No obstante, el BANCO DAVIVIENDA S.A. y sus\nFILIALES   no\nasumen ninguna responsabilidad por eventuales daños generados por\nel recibo y el uso de este material, siendo responsabilidad del destinatario\nverificar con sus propios medios la existencia de virus u otros\ndefectos. El presente correo electrónico solo refleja la opinión de\nsu Remitente y no representa necesariamente la opinión oficial del\nBANCO DAVIVIENDA S.A. y sus FILIALES  o de sus Directivos\n"
-    }
-    @valid_attrs %{payload: %{}, processed: true, raw: @email}
-    @update_attrs %{payload: %{}, processed: false, raw: @email}
-    @invalid_attrs %{payload: nil, processed: nil, raw: nil}
-
-    def notification_fixture(attrs \\ %{}) do
-      {:ok, notification} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Sources.create_notification()
-
-      notification
-    end
-
     test "list_notifications/0 returns all notifications" do
       notification = notification_fixture()
       assert Sources.list_notifications() == [notification]
@@ -106,6 +113,17 @@ defmodule Ynabit.SourcesTest do
       assert Sources.find_payee("FIRE HOUSE MILLA DE OR ") == {:ok, "FIRE HOUSE"}
       assert Sources.find_payee("Spotify P0adfadf") == {:ok, "Spotify"}
       assert Sources.find_payee("HEROKU *NOV-22222") == {:ok, "Heroku"}
+    end
+  end
+
+  describe "#post_notification_to_ynab" do
+    test "post transaction to YNAB" do
+      {:ok, notification} = Sources.parse_notification(@email)
+
+      use_cassette "new_transaction" do
+        assert {:ok, response} = Sources.post_notification_to_ynab(notification)
+        assert response.status_code == 201
+      end
     end
   end
 end
